2c675056c1d57318dc491f6ce5dc73b28a7ae14a
Letícia Gomes <leticia.gomes@vilt-group.com>
2026-01-20 15:19:30 +0100

    ✨Add business-cards component [2]

--- blocks/business-cards/business-cards.js
+++ blocks/business-cards/business-cards.js
@@ -1,27 +1,50 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
-  const cards = [...block.children];
-
   const ul = document.createElement('ul');
-  ul.className = 'business-cards__list';
 
-  cards.forEach((card) => {
+  [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    li.className = 'business-cards__item';
+    moveInstrumentation(row, li);
 
-    const picture = card.querySelector('picture');
-    const title = card.querySelector('h3');
-    const description = card.querySelector('p');
-    const link = card.querySelector('a');
+    const [image, title, description, link] = [...row.children];
+
+    if (image) {
+      image.className = 'business-cards__image';
+    }
+
+    if (title) {
+      title.className = 'business-cards__title';
+    }
+
+    if (description) {
+      description.className = 'business-cards__description';
+    }
 
     if (link) {
       link.className = 'business-cards__link';
-      link.append(picture, title, description);
-      li.append(link);
+    }
+
+    // move children into li
+    while (row.firstElementChild) {
+      li.append(row.firstElementChild);
     }
 
     ul.append(li);
   });
 
-  block.innerHTML = '';
-  block.append(ul);
+  // optimize images
+  ul.querySelectorAll('.business-cards__image picture > img').forEach((img) => {
+    const optimized = createOptimizedPicture(
+      img.src,
+      img.alt,
+      false,
+      [{ width: '800' }],
+    );
+    moveInstrumentation(img, optimized.querySelector('img'));
+    img.closest('picture').replaceWith(optimized);
+  });
+
+  block.replaceChildren(ul);
 }
